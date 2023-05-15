16744311c6a401c557bd0448fef15cb450f93e38
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-15 17:13:43 +0700

    Membuat kelas mahasiswa

--- staticfunction2_205/staticfunction2_205.cpp
+++ staticfunction2_205/staticfunction2_205.cpp
@@ -17,3 +17,32 @@ public:
 
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNIM(9); //mengakses nim melalui static member function "setnim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "Akses dari luar object = " << mahasiswa::getNim << endl;
+	system("Pause");
+
+	return 0;
+}
