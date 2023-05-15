45914e0f96d757e937234aa60a61138f9c1452fa
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-15 17:23:52 +0700

    data mahasiswa

--- staticfunction_205/staticfunction_205.cpp
+++ staticfunction_205/staticfunction_205.cpp
@@ -13,3 +13,28 @@ public:
 
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printALL() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+
+}
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+	return 0;
+}
